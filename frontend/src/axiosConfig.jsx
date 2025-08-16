cada1ba06556aff200ff4d0658a872c1e1df909d
Hmn3702 <nghia03072002@gmail.com>
2025-08-17 00:09:53 +0700

    CI/CD TESTING

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  // baseURL: 'http://localhost:5001', // local
   baseURL: 'http://3.105.255.76', // live
   headers: { 'Content-Type': 'application/json' },
 });
